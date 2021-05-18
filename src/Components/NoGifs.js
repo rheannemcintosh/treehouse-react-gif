50d9b7881df4e0ca887053a5c0ed25f14f3f29d0
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-05-18 19:01:45 +0100

    Format code

--- src/Components/NoGifs.js
+++ src/Components/NoGifs.js
@@ -1,10 +1,11 @@
-import React from 'react';
+
+import React from "react";
 
 const NoGifs = props => (
-  <li className='no-gifs'>
-    <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
-    <h3>Sorry, no GIFs match your search.</h3>
-  </li>
+	<li className="no-gifs">
+		<i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
+		<h3>Sorry, no GIFs match your search.</h3>
+	</li>
 );
 
 export default NoGifs;
